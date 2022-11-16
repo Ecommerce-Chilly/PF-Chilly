518b1be6a15da9860058b8de3201b152e3ea681b
cinthiapiera <cinthiapiera>
2022-11-16 18:42:15 -0500

    creacion modelo producto e invetario, camvio de variable index principal, creacion del index.js de routas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const app = require('./src/app.js');
-const { db } = require('./src/db.js');
+const { conn } = require('./src/db.js');
 const PORT = 3001;
 
-db.sync({force: true}).then(()=>{
+conn.sync({force: true}).then(()=>{
     app.listen(PORT,()=>{
         console.log(`Server listening on port ${PORT}`);
     });
