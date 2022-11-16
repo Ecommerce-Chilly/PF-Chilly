339a14761da333c3fec6c5988f2a18e4c06b8474
Esteban Cardona <estebanfabi737@gmail.com>
2022-11-16 18:30:13 -0500

    models of category and discount

---
+++ api/src/models/discount.js
@@ -0,0 +1,26 @@
+const sequelize = require("sequelize");
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define("category", {
+    id: {
+      type: DataTypes.INTEGER,
+      autuoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    percent: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+    },
+  });
+};
