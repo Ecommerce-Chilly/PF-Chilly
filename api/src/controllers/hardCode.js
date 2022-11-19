b71ff9dc4f80196b41142e84a2c42104cfd1d3cd
Esteban Cardona <estebanfabi737@gmail.com>
2022-11-18 21:07:42 -0500

    function create for the hardcode do it

--- api/src/controllers/hardCode.js
+++ api/src/controllers/hardCode.js
@@ -2,33 +2,29 @@ const { Product, Category, Inventory, Discount } = require("../db");
 const caseData = require("../dataApi/case.json");
 const postInvetory = require("./postInventory");
 
+const create = async function (el, datos = [], invCreate, categoryDB) {
+  const details = datos.map((element) => {
+    return { [element]: el[element] };
+  });
+  const proCreate = await Product.create({
+    name: el.title,
+    image: el.img,
+    price: el.price,
+    brand: el.brand,
+    model: el.model,
+    details: details,
+  });
+  await proCreate.setCategory(categoryDB);
+  await proCreate.setDiscount(null);
+  await invCreate.setProduct(proCreate);
+  await proCreate.setInventory(invCreate);
+};
 async function hardCodeoInfo() {
   const invCreate = await postInvetory(12);
   const categoryDB = await Category.findOne({ where: { name: "cases" } });
   for (let i = 0; i < caseData.length; i++) {
     const el = caseData[i];
-    const hola = async (el) => {
-      const proCreate = await Product.create({
-        name: el.title,
-        image: el.img,
-        price: el.price,
-        brand: el.brand,
-        model: el.model,
-        details: [
-          {
-            sidePanel: el.sidePanel,
-            color: el.color,
-            cabinetType: el.cabinetType,
-          },
-        ],
-      });
-      await proCreate.setCategory(categoryDB);
-      await proCreate.setDiscount(null);
-      await invCreate.setProduct(proCreate);
-      await proCreate.setInventory(invCreate);
-      console.log(proCreate);
-    };
-    hola(el);
+    create(el, ["sidePanel", "color", "cabinetType"], invCreate, categoryDB);
   }
   console.log(await Product.findAll());
 }
