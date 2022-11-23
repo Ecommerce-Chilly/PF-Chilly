6aa7974dea8eba9041688e887d30dc0c79f47989
Maxycarp89 <maxicostilla21@gmail.com>
2022-11-23 09:33:07 -0300

    about

--- api/src/controllers/getProducts.js
+++ api/src/controllers/getProducts.js
@@ -13,50 +13,47 @@ const { Product, Inventory } = require("../db");
 const { Product, Inventory } = require('../db');
 >>>>>>> 1b99f42275cfc9f02a242c29b4eeebb4d650a753
 
-const getProducts = async (category, id) => {
-  try {
-    if (id) {
-      const product = await Product.findByPk(id, {
-        include: {
-          model: Inventory,
-          attributes: ['quantity'],
-        },
-      });
-      if (!product) throw new Error(`Product with id ${id} is not exist`);
-      return product;
-    }
-    if (!category) {
-      const all = await Product.findAll({
-        include: {
-          model: Inventory,
-          attributes: ['quantity'],
-        },
-      });
-<<<<<<< HEAD
->>>>>>> 6563f1c97d3357dec1f376f60b1d8f71233b8a31
-=======
->>>>>>> 1b99f42275cfc9f02a242c29b4eeebb4d650a753
-      return all;
-    }
-    const prodGet = await Product.findAll({
-      where: { categoryName: category },
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1b99f42275cfc9f02a242c29b4eeebb4d650a753
-      include: {
-        model: Inventory,
-        attributes: ['quantity'],
-      },
-<<<<<<< HEAD
->>>>>>> 6563f1c97d3357dec1f376f60b1d8f71233b8a31
-=======
->>>>>>> 1b99f42275cfc9f02a242c29b4eeebb4d650a753
-    });
-    return prodGet;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+      async function getProducts(category, id) {
+        try {
+          if (id) {
+            const product = await Product.findByPk(id, {
+              include: {
+                model: Inventory,
+                attributes: ['quantity'],
+              },
+            });
+            if (!product)
+              throw new Error(`Product with id ${id} is not exist`);
+            return product;
+          }
+          if (!category) {
+            const all = await Product.findAll({
+              include: {
+                model: Inventory,
+                attributes: ['quantity'],
+              },
+            });
+
+
+
+
+            return all;
+          }
+          const prodGet = await Product.findAll({
+            where: { categoryName: category },
+
+
+
+
+
+            include: {
+              model: Inventory,
+              attributes: ['quantity'],
+            },
+          });
+          return prodGet;
+        } catch (error) {
+          throw new Error(error);
+        }
+      }
 module.exports = { getProducts };
--- client/src/Components/PI Components/Pages/AboutUS.jsx
+++ client/src/Components/PI Components/Pages/AboutUS.jsx
@@ -2,7 +2,54 @@ import React from 'react'
 
 function AboutUS() {
   return (
-    <div>AboutUS</div>
+    <div>
+      <h3 className ="" >aboutUs</h3>
+      <div >
+      <div >
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div className = "text-gray-700 text-center bg-gray-400 p-2">
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div>
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div>
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div>
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div>
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div>
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      <div>
+        <img src="https://www.optimaley.com/wp-content/uploads/2014/09/foto-perfil-generica.jpg" height="150px" alt="perfil" />
+        <h4>name</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, voluptas quod, cum eaque dicta nobis eos illum suscipit, soluta dolorum minus totam atque corrupti provident natus repudiandae vel! Est, quos.</p>
+      </div>
+      </div>
+
+    </div>
+     
+
   )
 }
 
