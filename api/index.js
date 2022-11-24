a3d2234e87b72e7bcd82d7011e817ab2d0f42dd4
juanrnoya <juanrnoya@gmail.com>
2022-11-24 11:40:12 -0300

    modificaciones

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const app = require("./src/app.js");
-// const { Category, Product, Invetory, Discount } = require("./src/db");
+// const { Category, Product, inventory, Discount } = require("./src/db");
 const { conn } = require("./src/db.js");
 const { getCategory } = require("./src/controllers/getCategory");
 const PORT = 3001;
 
 conn.sync({ force: true }).then(() => {
-  app.listen(PORT, () => {
-    getCategory();
-    console.log(`Server listening on port ${PORT}`);
-  });
+   app.listen(PORT, () => {
+      getCategory();
+      console.log(`Server listening on port ${PORT}`);
+   });
 });
