3aa584fe02b2ad19ded002e1c161002bc4e03807
juanrnoya <juanrnoya@gmail.com>
2022-11-26 13:58:20 -0300

    josemabranch_pull

--- api/src/app.js
+++ api/src/app.js
@@ -3,44 +3,48 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express();
-const routes = require('./routes/index.js');
-const cors = require('cors');
-const dotenv = require('dotenv')
-dotenv.config()
+const routes = require("./routes/index.js");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
-// Agrega credenciales
+//Agrega credenciales
 const { ACCESS_TOKEN } = process.env;
 
 mercadopago.configure({
-  access_token: ACCESS_TOKEN,
+   access_token: ACCESS_TOKEN,
 });
-require('./db.js');
 
-app.use(cors())
+require("./db.js");
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //req.body (el objeto contendrá valores de cualquier tipo y no solo cadenas)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+   res.header("Access-Control-Allow-Credentials", "true");
+   res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+   );
+   next();
 });
 
-app.use('/', routes);
+app.use("/", routes);
 
 //manejador de errores
 app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+   const status = err.status || 500;
+   const message = err.message || err;
+   console.error(err);
+   res.status(status).send(message);
 });
 
 module.exports = app;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,27 +3,18 @@ const productRoute = require("./productRoute");
 const inventoryRoute = require("./inventoryRoute");
 const discountsRoute = require("./discountsRoute");
 const detailsCategoryRoute = require("./detailsCategoryRoute");
-<<<<<<< HEAD
-const favoritesRoute = require("./favoriteRoute");
+const orderItemsRoute = require("./orderItemsRoute");
 const { hardCodeoInfo } = require("../controllers/hardCode");
-const { userRoute } = require("./userRoute");
-const { orderDetailRoutes } = require("./orderDetailsRoute");
-
-=======
-const orderItemsRoute = require("./orderItemsRoute")
-const { hardCodeoInfo } = require("../controllers/hardCode");
-const userRoute = require("./userRoute")
-const mercadoPago = require('./mercadoPago')
-const favoriteRoute = require('./favoriteRoute')
->>>>>>> 620647c6952243495d539c1e70f26b4f66e3e02a
+const userRoute = require("./userRoute");
+const mercadoPago = require("./mercadoPago");
+const favoriteRoute = require("./favoriteRoute");
 const router = Router();
 
 router.use("/Guillo", async (req, res, next) => {
-  // await defaultDiscount()
-  await hardCodeoInfo();
-  console.log("ya termine");
-  next();
-
+   // await defaultDiscount()
+   await hardCodeoInfo();
+   console.log("ya termine");
+   next();
 });
 
 router.use("/product", productRoute);
@@ -31,15 +22,9 @@ router.use("/product", productRoute);
 router.use("/discount", discountsRoute);
 router.use("/inventory", inventoryRoute);
 router.use("/categoryDetails", detailsCategoryRoute);
-<<<<<<< HEAD
 router.use("/user", userRoute);
-router.use("/favorite", favoritesRoute);
-router.use("/orderdetails", orderDetailsRoute);
-=======
-router.use("/user", userRoute)
-router.use("/favorite", favoriteRoute)
-router.use("/payment", mercadoPago)
-router.use("/orderItems", orderItemsRoute)
->>>>>>> 620647c6952243495d539c1e70f26b4f66e3e02a
+router.use("/favorite", favoriteRoute);
+router.use("/payment", mercadoPago);
+router.use("/orderItems", orderItemsRoute);
 
 module.exports = router;
