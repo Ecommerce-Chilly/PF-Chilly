ba9051f72b5c02c9ac2f92dc2fe0f0461ec47501
Guilloduran <guilloduran@gmail.com>
2022-11-28 13:25:36 -0300

    minor styling

--- client/src/Components/PI Components/Pages/AboutUS.jsx
+++ client/src/Components/PI Components/Pages/AboutUS.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function AboutUS() {
-  return (
-    <div>AboutUS</div>
-  )
+  return <div>AboutUS</div>;
 }
 
-export default AboutUS
+export default AboutUS;
