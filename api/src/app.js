398b561ae59037a45ffb7218fbc7649981dc87c2
Esteban Cardona <estebanfabi737@gmail.com>
2022-11-29 18:11:20 -0500

    ola ke ase

--- api/src/app.js
+++ api/src/app.js
@@ -7,6 +7,49 @@ const routes = require('./routes/index.js');
 const cors = require('cors');
 const dotenv = require('dotenv')
 dotenv.config()
+// const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+
+// // Authorization middleware. When used, the Access Token must
+// // exist and be verified against the Auth0 JSON Web Key Set.
+// const checkJwt = auth({
+//   audience: 'https://dev-r6cdo8stlhgup2wx.us.auth0.com/api/v2/',
+//   issuerBaseURL: `https://dev-r6cdo8stlhgup2wx.us.auth0.com/`,
+// });
+// var axios = require("axios").default;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://dev-r6cdo8stlhgup2wx.us.auth0.com/',
+//   headers: { authorization: 'Bearer YOUR_ACCESS_TOKEN_HERE' }
+// };
+
+// axios.request(options).then(function (response) {
+//   console.log(response.data);
+// }).catch(function (error) {
+//   console.error(error);
+// });
+// // This route doesn't need authentication
+// app.get('/api/public', function (req, res) {
+//   res.json({
+//     message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+//   });
+// });
+
+// // This route needs authentication
+// app.get('/api/private', checkJwt, function (req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+//   });
+// });
+
+// const checkScopes = requiredScopes('read:messages');
+
+// app.get('/api/private-scoped', checkJwt, checkScopes, function (req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+//   });
+// });
+
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
 // Agrega credenciales
