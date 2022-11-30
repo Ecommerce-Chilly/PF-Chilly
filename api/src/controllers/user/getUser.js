9b276775d44afe004567c00ce403b7a1dc1cf92f
dante-dlcio <erio-donalicio@hotmail.com>
2022-11-30 15:33:35 +0100

    "puliendo detalles"

--- api/src/controllers/user/getUser.js
+++ api/src/controllers/user/getUser.js
@@ -2,14 +2,15 @@ const { User, Order_items } = require("../../db")
 
 const getUser = async (id) => {
   try {
-    const usersById = await User.findAll({
-      where: { id: id },
+    if (!id) throw ("no id was found")
+    const usersById = await User.findByPk(id, {
       include: {
         model: Order_items,
-        attributes: ['id']
+        attributes: ['id', 'quantity']
       }
     },
     )
+    if (!usersById) throw new Error("User not found")
     return usersById
   } catch (error) {
     console.log(error)
@@ -19,6 +20,7 @@ const getUser = async (id) => {
 const getAllUsers = async () => {
   try {
     const allUsers = await User.findAll()
+    if (allUsers.length === 0) throw "no users logged in the Data Base"
     return allUsers
   } catch (error) {
     console.log(error)
