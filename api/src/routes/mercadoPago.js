9b6005c291551978f53b3822ae2a34116f9a1c37
Guilloduran <guilloduran@gmail.com>
2022-12-02 18:23:05 -0300

    Merged with esteab branch

--- api/src/routes/mercadoPago.js
+++ api/src/routes/mercadoPago.js
@@ -8,15 +8,5 @@ const { checkJwt } = require('../middleware/oAuth');
 mercadoPago.post('/', checkJwt, async (req, res) => {
   paymentInstace.getPaymentLink(req, res);
 });
-const { Router } = require('express');
-const PaymentController = require('../controllers/mercadoPago');
-const PaymentServices = require('../services/mercadoPago');
-const mercadoPago = Router();
-const paymentInstace = new PaymentController(new PaymentServices());
-const { checkJwt } = require('../middleware/oAuth');
-
-mercadoPago.post('/', checkJwt, async (req, res) => {
-  paymentInstace.getPaymentLink(req, res);
-});
 
 module.exports = mercadoPago;
