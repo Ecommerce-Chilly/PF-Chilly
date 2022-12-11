8600e1147d22c37e2445094fe738b2235a2b7699
dante-dlcio <erio-donalicio@hotmail.com>
2022-12-11 20:40:36 +0100

    "Cart, Cart_Items routes, posts, gets, deltes and modifiers done"

---
+++ api/src/controllers/cart_items/modifyCartItems.js
@@ -0,0 +1,19 @@
+const { Cart_items } = require("../../db")
+
+const modifyQuantity = async (id, quantity) => {
+    try {
+        if (!id) throw "no id found"
+        const cartItems = await Cart_items.findByPk(id)
+        if (!cartItems) throw "no cart_items found"
+        if (cartItems) cartItems.quantity = quantity
+        await cartItems.save()
+        return cartItems
+
+
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = { modifyQuantity }
--- api/src/routes/cartItemsRoute.js
+++ api/src/routes/cartItemsRoute.js
@@ -1,5 +1,6 @@
 const { Router } = require("express")
 const { postCartItems } = require("../controllers/cart_items/postCartItems")
+const { modifyQuantity } = require("../controllers/cart_items/modifyCartItems")
 
 
 const cartItemsRoute = Router()
@@ -10,6 +11,18 @@ cartItemsRoute.post('/', async (req, res) => {
         res.status(201).send(cart_items)
     } catch (error) {
         res.status(404).send(error)
+    }
+})
+
+cartItemsRoute.put('/', async (req, res) => {
+    try {
+        const id = req.body.id
+        const quantity = req.body.quantity
+        const updateQuantity = await modifyQuantity(id, quantity)
+        res.status(202).send(updateQuantity)
+    } catch (error) {
+        res.status(304).send({ msg: "not modified" }, error)
+
     }
 })
 
