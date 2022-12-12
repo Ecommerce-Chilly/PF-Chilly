c8abbf997966ed0ca1176e100f8f42701fefaa97
JosemaCeballos <josema.soyhenry@gmail.com>
2022-12-11 21:22:06 -0300

    contact us

--- client/src/Components/PI Components/Pages/AboutUS.jsx
+++ client/src/Components/PI Components/Pages/AboutUS.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Accordeon from './Accordeon';
-import AboutCard from './AboutCard';
+import React from "react";
+import Accordeon from "./Accordeon";
+import AboutCard from "./AboutCard";
+import Mailer from "../../Mailer/Mailer";
 
 function AboutUS() {
   return (
@@ -10,16 +11,17 @@ function AboutUS() {
       </h2>
 
       <div class="flex w-11/12 flex-wrap justify-evenly mb-10 mx-auto">
-        <AboutCard name={'Jose Maria Ceballos'} />
-        <AboutCard name={'Guillermo Duran'} />
-        <AboutCard name={'Jesus Torrecilla'} />
-        <AboutCard name={'Esteban Wonder'} />
-        <AboutCard name={'Cinthia Maldonado'} />
-        <AboutCard name={'Maximiliano Costilla'} />
-        <AboutCard name={'Dante Erio Donalicio'} />
-        <AboutCard name={'Juan Noya'} />
+        <AboutCard name={"Jose Maria Ceballos"} />
+        <AboutCard name={"Guillermo Duran"} />
+        <AboutCard name={"Jesus Torrecilla"} />
+        <AboutCard name={"Esteban Wonder"} />
+        <AboutCard name={"Cinthia Maldonado"} />
+        <AboutCard name={"Maximiliano Costilla"} />
+        <AboutCard name={"Dante Erio Donalicio"} />
+        <AboutCard name={"Juan Noya"} />
       </div>
       <Accordeon />
+      <Mailer />
     </div>
   );
 }
