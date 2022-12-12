101d9bb691cead3abb6bed7477ae1ca3ca427e68
JosemaCeballos <josema.soyhenry@gmail.com>
2022-12-12 20:17:21 -0300

    another commit

--- api/src/controllers/cart_items/modifyCartItems.js
+++ api/src/controllers/cart_items/modifyCartItems.js
@@ -1,19 +1,16 @@
-const { Cart_items } = require("../../db")
+const { Cart_items } = require("../../db");
 
 const modifyQuantity = async (id, quantity) => {
-    try {
-        if (!id) throw "no id found"
-        const cartItems = await Cart_items.findByPk(id)
-        if (!cartItems) throw "no cart_items found"
-        if (cartItems) cartItems.quantity = quantity
-        await cartItems.save()
-        return cartItems
+  try {
+    if (!id) throw "no id found";
+    const cartItems = await Cart_items.findByPk(id);
+    if (!cartItems) throw "no cart_items found";
+    if (cartItems) cartItems.quantity = quantity;
+    await cartItems.save();
+    return cartItems;
+  } catch (error) {
+    throw error;
+  }
+};
 
-
-
-    } catch (error) {
-
-    }
-}
-
-module.exports = { modifyQuantity }
+module.exports = { modifyQuantity };
