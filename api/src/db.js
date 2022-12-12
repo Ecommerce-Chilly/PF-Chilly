a606b65e2c55655fabc432962421a689dcba4d6d
cinthiapiera <cinthiapiera>
2022-12-12 16:14:48 -0500

    update back 12/12

--- api/src/db.js
+++ api/src/db.js
@@ -5,19 +5,19 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/chilly`,
-//   {
-//     logging: false,
-//     native: false,
-//   }
-// );
-const sequelize = new Sequelize(DB_DEPLOY,
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/chilly`,
   {
     logging: false,
     native: false,
   }
 );
+// const sequelize = new Sequelize(DB_DEPLOY,
+//   {
+//     logging: false,
+//     native: false,
+//   }
+// );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
