2a1575a6b462bcc528e3a8ea1619513d6d5a6130
Esteban Cardona <estebanfabi737@gmail.com>
2022-12-13 21:06:48 -0500

    data user resolved

--- api/src/models/data_user.js
+++ api/src/models/data_user.js
@@ -1,42 +1,46 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
-   sequelize.define(
-      "data_user",
-      {
-         id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false,
-            unique: true,
-         },
-         direction: {
-            type: DataTypes.STRING,
-            validate: { isAlphanumeric: true },
-         },
-         dni: {
-            type: DataTypes.STRING,
-            validate: { isAlphanumeric: true },
-         },
-         zip: {
-            type: DataTypes.INTEGER,
-            validate: { isAlphanumeric: true },
-         },
-         country: {
-            type: DataTypes.STRING,
-            validate: { isAlpha: true },
-         },
-         dep_prov: {
-            type: DataTypes.STRING,
-            validate: { isAlpha: true },
-         },
-         city: {
-            type: DataTypes.STRING,
-            validate: { isAlpha: true },
-         },
+  sequelize.define(
+    "data_user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
       },
-      {
-         timestamps: false,
-      }
-   );
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      country: {
+        type: DataTypes.STRING,
+        validate: { isAlpha: true },
+      },
+      city: {
+        type: DataTypes.STRING,
+        validate: { isAlpha: true },
+      },
+      postalcode: {
+        type: DataTypes.INTEGER,
+        validate: { isNumeric: true, len: [1, 6] },
+      },
+      mobile: {
+        type: DataTypes.INTEGER,
+        validate: { isNumeric: true, len: [1, 11] },
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
