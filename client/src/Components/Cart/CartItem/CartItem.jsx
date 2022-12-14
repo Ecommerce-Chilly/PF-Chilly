2d0d33ef0c1685de6db56ad1df4cac3aabe2ed44
Esteban Cardona <estebanfabi737@gmail.com>
2022-12-13 20:55:16 -0500

    bugs resolved of cart

--- client/src/Components/Cart/CartItem/CartItem.jsx
+++ client/src/Components/Cart/CartItem/CartItem.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   deleteP,
   increaseProductQuantity,
@@ -8,7 +8,8 @@ import {
   deleteFromCartBack,
   getCartFromBack,
   putCartFromBack,
-} from '../../../redux/actions/actions';
+} from "../../../redux/actions/actions";
+import store from "../../../redux/store/store";
 
 function CartItem({ name, quantity, price, image, id, changeVariable }) {
   const dispatch = useDispatch();
@@ -23,8 +24,23 @@ function CartItem({ name, quantity, price, image, id, changeVariable }) {
       dispatch(getCartFromBack(userInfo.id));
     }, 2000);
   };
+  const changeModify = async (e) => {
+    if (e.target.value === "-") {
+      dispatch(decreaseProductQuantity(id));
+      let quantity = store.getState().quantity;
+      console.log(quantity);
+      await dispatch(putCartFromBack(ostras[0].id, id, quantity));
+      await dispatch(getCartFromBack(userInfo.id));
+    }
+    if (e.target.value === "+") {
+      dispatch(increaseProductQuantity(id));
+      let quantity = store.getState().quantity;
+      await dispatch(putCartFromBack(ostras[0].id, id, quantity));
+      await dispatch(getCartFromBack(userInfo.id));
+    }
+  };
   React.useEffect(() => {
-    console.log('hola');
+    console.log("hola");
   }, [msgPuto]);
 
   React.useEffect(() => {
@@ -36,7 +52,7 @@ function CartItem({ name, quantity, price, image, id, changeVariable }) {
       <div className="flex justify-between items-center h-52 w-full">
         <div className="flex  items-center   h-full w-3/4  mr-4 ">
           <div className="h-full w-60 ">
-            <img src={image.replace('SL75', 'SL500')} class=" h-full m-auto" />
+            <img src={image.replace("SL75", "SL500")} class=" h-full m-auto" />
           </div>
           <div className="flex flex-col ml-3 w-3/5">
             <span className="md:text-md font-medium">{name}</span>
@@ -45,36 +61,26 @@ function CartItem({ name, quantity, price, image, id, changeVariable }) {
 
         <div className="flex justify-between  items-center   w-60">
           <div className="  flex text-base flex-row items-center ">
-            <span
-              onClick={() => {
-                dispatch(decreaseProductQuantity(id));
-                dispatch(putCartFromBack(ostras[0].id, id, quantity));
-                setTimeout(() => {
-                  dispatch(getCartFromBack(userInfo.id));
-                }, 10000);
-              }}
+            <button
+              value="-"
+              onClick={changeModify}
               className="font-semibold w-9 rounded  text-center cursor-pointer text-lg hover:bg-main hover:text-white"
             >
               -
-            </span>
+            </button>
             <input
               type="text"
               className="focus:outline-none bg-gray-100  h-6 w-10  rounded text-center  px-2 mx-2 text-lg"
               value={quantity}
               disabled
             />
-            <span
-              onClick={() => {
-                dispatch(increaseProductQuantity(id));
-                dispatch(putCartFromBack(ostras[0].id, id, quantity));
-                setTimeout(() => {
-                  dispatch(getCartFromBack(userInfo.id));
-                }, 10000);
-              }}
+            <button
+              value="+"
+              onClick={changeModify}
               className="font-semibold w-9 rounded  text-center cursor-pointer text-lg hover:bg-main hover:text-white"
             >
               +
-            </span>
+            </button>
           </div>
           <div className="flex items-center justify-between ">
             <div className="mr-10 w-16    ml-6">
