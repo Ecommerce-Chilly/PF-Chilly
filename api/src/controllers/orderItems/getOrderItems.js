d91060bd2c805c985f439458568ceb8f8e08af3b
Guillermo Duran <guilloduran@gmail.com>
2022-12-15 13:25:32 -0300

    Update getOrderItems.js

--- api/src/controllers/orderItems/getOrderItems.js
+++ api/src/controllers/orderItems/getOrderItems.js
@@ -1,22 +1,33 @@
-const { Order_items } = require("../../db")
+const { Order_items, User } = require('../../db');
 
 const getOrderItems = async (id) => {
   try {
-    if (!id) throw "no Id found"
-    const OrderItemsById = await Order_items.findByPk(id)
-    return OrderItemsById
+    if (!id) throw 'no Id found';
+    const OrderItemsById = await Order_items.findByPk(id, {
+      include: {
+        model: User,
+        attributes: ['email', 'name'],
+      },
+    });
+
+    return OrderItemsById;
   } catch (error) {
-    throw (error)
+    throw error;
   }
-}
+};
 
 const getAllOrders = async () => {
   try {
-    const allUsers = await Order_items.findAll()
-    return allUsers
+    const allUsers = await Order_items.findAll({
+      include: {
+        model: User,
+        attributes: ['email', 'name'],
+      },
+    });
+    return allUsers;
   } catch (error) {
-    throw (error)
+    throw error;
   }
-}
+};
 
-module.exports = { getOrderItems, getAllOrders }
+module.exports = { getOrderItems, getAllOrders };
