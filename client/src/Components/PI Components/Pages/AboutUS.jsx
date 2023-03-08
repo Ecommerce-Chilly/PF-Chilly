a73a5bc89e38f216d7f6ebd7a58dcf1a709913c9
Esteban Cardona <estebanfabi737@gmail.com>
2023-03-07 20:44:50 -0500

    Update the perfil photos of about us 😎

--- client/src/Components/PI Components/Pages/AboutUS.jsx
+++ client/src/Components/PI Components/Pages/AboutUS.jsx
@@ -6,27 +6,27 @@ import AboutCard from "./AboutCard";
 function AboutUS() {
   let infoShino = {
     text: "Hello, i'm from Argentina. I prefer working in front, but i also know backend. I'm good working with people and i like to learn everything i can. Feel free to contact me!",
-    img: 'https://media.licdn.com/dms/image/D4D35AQHd1uduTJKPbA/profile-framedphoto-shrink_800_800/0/1672345454131?e=1676959200&v=beta&t=xoQWKszs1YH7grkC3jNuInBEFnaUjvZR1hmPEXQLhAQ',
+    img: 'https://media.licdn.com/dms/image/D4D35AQHd1uduTJKPbA/profile-framedphoto-shrink_200_200/0/1672345454131?e=1678845600&v=beta&t=hBh3FvgHXwwOd_uZ_95GJlmAUQkRF_AmdrrvK3XFEIw',
     url: 'https://www.linkedin.com/in/jes%C3%BAs-torrecilla-4324b9214/',
   };
   let infoJose = {
     text: 'Hi, my name is José, I am from Argentina, and I have a strong preference for front-end work, but I also have knowledge in the back end. Contact me on LinkedIn.',
-    img: 'https://media.licdn.com/dms/image/D4D03AQHFelWOQYrqfg/profile-displayphoto-shrink_800_800/0/1663561013663?e=1676505600&v=beta&t=EVJhs_Wes7mz05-vDrc-w4OV6Ari6KS3ll1uYwUgtks',
+    img: 'https://media.licdn.com/dms/image/D4D35AQGYp-LAu4hqQA/profile-framedphoto-shrink_200_200/0/1672695453095?e=1678845600&v=beta&t=r6KptHfEwya6j91ODFK9x_QuP7KwttAEDl9zB_LI2RM',
     url: 'https://www.linkedin.com/in/jose-maria-ceballos-a3379524a/',
   };
   let infoCinthia = {
     text: "Hello! my name is Cinthia Maldonado. I enjoy working in a team, collaborating and bringing value. I'm dedicated, proactive and positive. ",
-    img: 'https://media.licdn.com/dms/image/D4E03AQGGkAkZSZuWvA/profile-displayphoto-shrink_800_800/0/1672883835627?e=1681948800&v=beta&t=0hjqAGLgMYE5BX8euMWn9LFFLvgotf2k6xhhasoekcQ',
+    img: 'https://media.licdn.com/dms/image/D4E03AQGGkAkZSZuWvA/profile-displayphoto-shrink_200_200/0/1672883835627?e=1683763200&v=beta&t=cjtqxMddA1gvPrCBsF-EjxsJvBEIXK1eIp_b3AUwbVw',
     url: 'https://www.linkedin.com/in/cinthia-maldonado-rafael/',
   };
   let infoMaxi = {
     text: "Hi, I'm from Tucumán Argentina, I like achilatas and milanesa sandwiches.",
-    img: 'https://media.licdn.com/dms/image/D4D35AQHnwQa3EUCaQw/profile-framedphoto-shrink_800_800/0/1672188839498?e=1676959200&v=beta&t=-uTzdbK5ITkWowfJRL1_9Vm-AMguASenpK5iMKbRPps',
+    img: 'https://media.licdn.com/dms/image/D4D35AQHnwQa3EUCaQw/profile-framedphoto-shrink_200_200/0/1672188839498?e=1678845600&v=beta&t=TliD5vdMZG-0HeVIqpbmtHt_t7uOEnT1ZEIZvkbUooI',
     url: 'https://www.linkedin.com/in/maximiliano-costilla-1805b4213/',
   };
   let infoDante = {
     text: "This dev student is a coding wizard with 8 magical cats. They're unstoppable when it comes to tackling tough projects and defeating evil videogame bosses.",
-    img: 'https://media.licdn.com/dms/image/D4D35AQGH4DdhV2bXGA/profile-framedphoto-shrink_800_800/0/1633987157969?e=1676959200&v=beta&t=2v_Y7XZts4s49laIp5LoGYmTiYPJZTkPD-HZ28-1-IM',
+    img: 'https://media.licdn.com/dms/image/D4D35AQGH4DdhV2bXGA/profile-framedphoto-shrink_200_200/0/1633987157969?e=1678845600&v=beta&t=nZaaIJbp9wZKwUkl-YDLyih0Zw0OVnGkYlCWVHVoYxA',
     url: 'https://www.linkedin.com/in/erio-donalicio-gimenez/',
   };
   let infoGuillo = {
@@ -36,7 +36,7 @@ function AboutUS() {
   };
   let infoEsteban = {
     text: "Hey, I'm from Colombia and I'm the backend guy, I really like to solve logic problems and work with friends because it's a lot of fun.",
-    img: 'https://cdn.discordapp.com/attachments/1016546130877763624/1052673237689049178/6d91dc6f-81f1-4658-b07d-f0b6e508e7a0.jpg',
+    img: 'https://media.licdn.com/dms/image/D4E35AQEyJP4Aqr2-1g/profile-framedphoto-shrink_200_200/0/1672692886395?e=1678845600&v=beta&t=OScZpkcO6xqwOkQXgeCSAjjo8ziF_-GEex-9YxSHKb4',
     url: 'https://www.linkedin.com/in/estebancardonasarria/',
   };
   let infoCoco = {
